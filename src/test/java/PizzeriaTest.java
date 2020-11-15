c9facda7d25281719273643a4d1b11894236a2e2
JMB <jbruel@gmail.com>
2020-11-15 14:06:57 +0100

    imrpving tests

--- src/test/java/PizzeriaTest.java
+++ src/test/java/PizzeriaTest.java
@@ -8,15 +8,15 @@ public class PizzeriaTest {
     public void anOrderedPizzaIsNotNull() {
         Pizzeria boutiqueBrest = new Pizzeria();
         Pizza miam = boutiqueBrest.commanderPizza("cheese");
-        assertNotNull("An Ordered Pizza Is Not Null",miam == null);
+        assertNotNull("An Ordered Pizza Is Not Null",miam);
     }
 
     @Test
     public void twoOrderedPizzasAreDifferent() {
         Pizzeria boutiqueBrest = new Pizzeria();
         Pizzeria boutiqueStrasbourg = new Pizzeria();
-        Pizza miam = boutiqueBrest.commanderPizza("cheese");
+        Pizza miam1 = boutiqueBrest.commanderPizza("cheese");
         Pizza miam2 = boutiqueStrasbourg.commanderPizza ("Greek");
-        assertFalse("Two Ordered Pizzas Are Different",miam.equals(miam2));
+        assertNotEquals("Two Ordered Pizzas Are Different",miam1,miam2);
     }
 }
